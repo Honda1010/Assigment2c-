1e1b1dff1b70dc4fccf1be1e97fe6fb0788e960c
Mohaned <mohanedmohamed267@gmail.com>
2025-07-04 21:42:07 +0300

    Initial commit without .vs folder

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿namespace Ass3Course
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Question 1
+			/**
+			 * Write a program that takes a number from the user 
+			 * then print yes if that number can be divided by 3 and 4 otherwise print no
+			 */
+			//Console.WriteLine("Enter a number:");
+			//int number = int.Parse(Console.ReadLine());
+			//if (number % 3 == 0 && number % 4 == 0)
+			//{
+			//	Console.WriteLine("Yes");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("No");
+			//}
+			#endregion
+			#region Question 2
+			/**
+			 * Write a program that allows the user to insert an integer 
+			 * then print negative if it is negative number otherwise print positive
+			 */
+			//Console.Write("Enter an integer: ");
+			//int integer = int.Parse(Console.ReadLine());
+			//if (integer < 0)
+			//{
+			//	Console.WriteLine("Negative");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Positive");
+			//}
+			#endregion
+			#region Question 3
+			/**
+			 * Write a program that takes 3 integers from the user 
+			 * then prints the max element and the min element
+			 */
+			//Console.Write("Enter first Number: ");
+			//int firstNum = int.Parse(Console.ReadLine());
+			//Console.Write("Enter second Number: ");
+			//int secondNum = int.Parse(Console.ReadLine());
+			//Console.Write("Enter third Number: ");
+			//int thirdNum = int.Parse(Console.ReadLine());
+			//int max = firstNum;
+			//int min = firstNum;
+			//if (secondNum > max)
+			//{
+			//	max = secondNum;
+			//}
+			//if (thirdNum > max)
+			//{
+			//	max = thirdNum;
+			//}
+			//if (secondNum < min)
+			//{
+			//	min = secondNum;
+			//}
+			//if (thirdNum < min)
+			//{
+			//	min = thirdNum;
+			//}
+			//Console.WriteLine($"Max: {max}, Min: {min}");
+			#endregion
+			#region Question 4
+			/**
+			 * Write a program that allows the user to insert an integer number 
+			 * then check If a number is even or odd
+			 */
+			//Console.Write("Enter an number: ");
+			//int Number = int.Parse(Console.ReadLine());
+			//if (Number % 2 == 0)
+			//{
+			//	Console.WriteLine("Even");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Odd");
+			//}
+			#endregion
+			#region Question 5
+			/**
+			 * Write a program that takes character from the user 
+			 * then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant)
+			 */
+			//Console.Write("Enter a character: ");
+			//char ch = char.Parse(Console.ReadLine().ToLower());
+			//if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+			//{
+			//	Console.WriteLine("Vowel");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Consonant");
+			//}
+			#endregion
+
+		}
+	}
+}
