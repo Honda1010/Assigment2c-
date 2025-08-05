9ca21a5b234c8e3531590a069b0bf1f1a7ffeb5f
Mohaned <mohanedmohamed267@gmail.com>
2025-08-05 23:49:39 +0300

    Upload Ass

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using ADVAss1.Question2;
+namespace ADVAss1
+{
+	internal class Program
+	{
+		#region Question1
+		/*
+		 * The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets.
+		 * How we can optimize the Bubble Sort algorithm And implement the code of this optimized bubble sort algorithm
+		 */
+		public static void OptimizedBubbleSort(int[] arr)
+		{
+			int n = arr.Length;
+			bool swapped;
+			for (int i = 0; i < n - 1; i++)
+			{
+				swapped = false;
+				for (int j = 0; j < n - i - 1; j++)
+				{
+					if (arr[j] > arr[j + 1])
+					{
+						int temp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
+						swapped = true;
+					}
+				}
+				if (!swapped)
+					break;
+			}
+		}
+		#endregion
+		static void Main(string[] args)
+		{
+			#region TestQuestion1
+			int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+			Console.WriteLine("Original array:");
+			Console.WriteLine(string.Join(", ", arr));
+			OptimizedBubbleSort(arr);
+			Console.WriteLine("Sorted array:");
+			Console.WriteLine(string.Join(", ", arr));
+			#endregion
+			#region TestQuestion2
+			Range<int> intRange = new Range<int>(10, 20);
+			Console.WriteLine(intRange);
+			int valueToCheck=15;
+			Console.WriteLine($"Is {valueToCheck} in range? {intRange.IsInRange(valueToCheck)}");
+			Console.WriteLine($"Length of range: {intRange.Length()}");
+			Range<double> doubleRange = new Range<double>(5.5, 10.5);
+			Console.WriteLine(doubleRange);
+			double doubleValueToCheck = 7.5;
+			Console.WriteLine($"Is {doubleValueToCheck} in range? {doubleRange.IsInRange(doubleValueToCheck)}");
+			Console.WriteLine($"Length of range: {doubleRange.Length()}");
+			#endregion
+
+
+		}
+	}
+}
---
+++ Question2/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADVAss1.Question2
+{
+	internal class Range<T> where T : IComparable<T>
+	{
+        public T min { get; set; }
+		public T max { get; set; }
+        public Range(T _min,T _max)
+        {
+			min = _min;
+			max = _max;
+		}
+		public bool IsInRange(T value)
+		{
+			return value.CompareTo(min) >= 0 &&
+				   value.CompareTo(max) <= 0;
+		}
+		public Double Length()
+		{
+			return Convert.ToDouble(max)- Convert.ToDouble(min);
+		}
+		public override string ToString()
+		{
+			return $"Range: [{min}, {max}]";
+		}
+
+	}
+}
