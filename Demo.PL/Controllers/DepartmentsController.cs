d569fc6d125717a5a08af8ac2b86af2c1de2d7c6
Mohaned <mohanedmohamed267@gmail.com>
2025-10-04 08:19:04 +0300

    update ass v3.0

--- Demo.PL/Controllers/DepartmentsController.cs
+++ Demo.PL/Controllers/DepartmentsController.cs
@@ -1,4 +1,5 @@
-﻿using Demo.BL2.Services;
+﻿using Demo.BL2.DTOS;
+using Demo.BL2.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.PL.Controllers
@@ -10,5 +11,27 @@ namespace Demo.PL.Controllers
 			var departments = _departmentService.GetAll();
 			return View(departments);
 		}
+		#region Create
+		[HttpGet]
+		public IActionResult Create()
+		{
+				return View();
+		}
+		[HttpPost]
+		public IActionResult Create(CreatedDepartmentDto createdDepartmentDto)
+		{
+			if (ModelState.IsValid)
+			{
+				_departmentService.Add(createdDepartmentDto);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View(createdDepartmentDto);
+
+			}
+		}
+
+		#endregion
 	}
 }
